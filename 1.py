c4bbe9cf887c9b33e441b51cd2498db09f2de007
Arthur142152 <artur.chuprina.2010@gmail.com>
2025-03-08 14:26:47 +0200

    Додано 13 рядків

--- 1.py
+++ 1.py
@@ -1,1 +1,13 @@
-print("Hello world")
+print("\033[36m\033[1mПідтвердження особистості")
+print("="*42,"\033[0m\n")
+age=int(input("how old are you:"))
+    print("\033[36m\033[1m")
+if age >=0 and age <14:
+    print("Свідоцво про народження")
+elif 14<=age<=35
+    print("id картка")
+elif 35<age<=110:
+    print("паспорт старого зразку")
+else:
+    print("помилке при введены віку")
+    print("\033[0m")
