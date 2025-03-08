8f2de58a463b915f8b6894fc32395294c87eb6ae
Arthur142152 <artur.chuprina.2010@gmail.com>
2025-03-08 14:54:40 +0200

    додано цикл

--- 1.py
+++ 1.py
@@ -1,13 +1,17 @@
 print("\033[36m\033[1mПідтвердження особистості")
 print("="*42,"\033[0m\n")
-age=int(input("how old are you:"))
-print("\033[36m\033[1m")
-if age >=0 and age <14:
-    print("Свідоцво про народження")
-elif 14<=age<=35:
-    print("id картка")
-elif 35<age<=110:
-    print("паспорт старого зразку")
-else:
-    print("помилке при введены віку")
-print("\033[0m")
+#while ans=="1":
+    #age=int(input("how old are you:"))
+    for kol in range (3):
+    print("\033[36m\033[1m")
+    if age >=0 and age <14:
+        print("Свідоцво про народження")
+    elif 14<=age<=35:
+        print("id картка")
+    elif 35<age<=110:
+        print("паспорт старого зразку")
+    else:
+        print("помилке при введеня віку")
+    print("\033[0m")
+    ans=input("продовжити:(1-так,2-ні)")
+print("Програма закінчилась")
